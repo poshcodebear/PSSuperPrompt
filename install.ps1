13743ad50cafac32d1f3dee08c27881c164eac64
The PowerShell Bear <poshcodebear@outlook.com>
2024-01-30 13:39:26 -0800

    Create script with workflow description

---
+++ install.ps1
@@ -0,0 +1,6 @@
+<# Workflow:
+- Verify profile exists
+    - Create profile and profile path if necessary
+- Copy prompt.ps1 to profile path
+- Add a line at the end of the profile to dot-source the prompt
+#>
